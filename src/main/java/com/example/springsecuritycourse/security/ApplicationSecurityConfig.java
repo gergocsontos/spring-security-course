143a1d7c6bdc267c1f07dd24f1a0eb102eee6d48
gergocsontos <gergo.csontos.123@gmail.com>
2022-02-07 19:03:01 +0100

    21 - Remember Me Cookie And Extra Options

--- src/main/java/com/example/springsecuritycourse/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/springsecuritycourse/security/ApplicationSecurityConfig.java
@@ -13,6 +13,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+import java.util.concurrent.TimeUnit;
+
 import static com.example.springsecuritycourse.security.ApplicationUserRole.*;
 
 @Configuration
@@ -42,7 +44,9 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .loginPage("/login").permitAll()
                 .defaultSuccessUrl("/courses", true)
                 .and()
-                .rememberMe();
+                .rememberMe()
+                    .tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(21))
+                    .key("somethingverysecured");
     }
 
     @Bean
